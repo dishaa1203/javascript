8279d82e12e0637b0c0e17a13da7472620676c91
dishaa1203 <disha12032005@gmail.com>
2026-01-02 22:06:43 +0530

    Add files via upload

---
+++ basic/conversionOperation.js
@@ -0,0 +1,47 @@
+let score = "disha"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+//"33" => 33
+//"33snd" =>NaN
+//true =>1
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true , 0 => false
+//"" => false
+//"disha" => true
+let someNumber=12
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//operations
+
+let value = 4
+let negValue = -value
+console.log(negValue);
+
+console.log(37+32);
+console.log(37-32);
+console.log(37%32);
+console.log(37*32);
+console.log(37**32);
+console.log(37/32);
+
+
+
+
+
+
+
