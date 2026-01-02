e0fcf91d3faac34f9411e8a0ae2e310a85209730
dishaa1203 <disha12032005@gmail.com>
2026-01-02 07:51:17 +0000

    dataTypes

---
+++ basic/dataTypes.js
@@ -0,0 +1,75 @@
+"use strict"; // treat all JS code as newer version
+
+//alert()  //we are using node.js not browser
+
+console.log("disha")// code readability shoul be high 
+
+console.log(4+6)
+
+let name ="disha"
+let age = 20
+let isLoggedIn = true
+
+//primitive data types
+//number => 2 to power 53
+//bigint if high number
+//string =>""
+//boolean => true/false
+//null =>standalone value 
+//undefined => value not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof age) //number 
+console.log(typeof name); //string
+console.log(typeof null); //object
+console.log(typeof undefined) //undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ basic/variables.js
@@ -0,0 +1,33 @@
+const accountId = 1203 
+let accountEmail = "disha1203@gmail.com"
+var accountPassword = "1203"
+accountCity = "Pune"
+let accountState;
+//accountId = 4569 // not allowed
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+
+accountEmail ="disha4321@gmail.com"
+
+accountPassword = "9030"
+
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState])
+
+
+
+
+
+
+
