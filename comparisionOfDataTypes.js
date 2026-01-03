657e08aee9769fba1d972b564a9b93ee59a50d93
dishaa1203 <disha12032005@gmail.com>
2026-01-03 23:20:21 +0530

    Add files via upload
    Comparison of data types

---
+++ comparisionOfDataTypes.js
@@ -0,0 +1,53 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+
+console.log("02" > 1);
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true because null is converted to 0 or NaN
+
+
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+//avoid conversions of null and undefined
+// strict check also check datatypes
+
+console.log("2" === 2);//false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
